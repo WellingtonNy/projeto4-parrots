b1e5c2e53496320624acd599ac37d43e45a57468
Wellington <Wellingtoncpfilho@gmail.com>
2023-01-16 17:35:30 -0300

    implementado logica principal

--- script.js
+++ script.js
@@ -1,37 +1,70 @@
+let numeroDeCartas= 0
 let controleDeClicks = 0
+let controleDeClicksCertos=0
 let controleDeImg = ''
-let testando = 'n'
-console.log(controleDeImg);
-
+let classeControle1;
+let classeControle2;
+let back;
+let front;
+let back2;
+let front2;
 
 function NaCarta (elemento) {
     controleDeClicks++
     elemento.classList.add('gambiarra');
     elemento.classList.toggle('virar');
     let srcImg =document.querySelector('.gambiarra>.front').src.toString();
+    setTimeout(virar,270,elemento);  
    
     if(controleDeImg === ''){
         controleDeImg = srcImg
+        elemento.classList.add('controle1')
+        elemento.classList.add('desabilitar')
+        classeControle1=document.querySelector('.controle1')
+        back2 = back
+        front2 = front
     }else{
         if(controleDeImg === srcImg){
-          testando='s'
+          elemento.classList.add('controle2');
+          elemento.classList.add('desabilitar')
+          classeControle2=document.querySelector('.controle2')
+          classeControle1.classList.remove('controle1');
+          classeControle2.classList.remove('controle2');
           controleDeImg = ''
+          controleDeClicksCertos++
+          console.log(controleDeClicksCertos);
         }else{
-            testando='n'
+            elemento.classList.add('controle2');
+            classeControle2 =document.querySelector('.controle2')
+            classeControle1.classList.remove('desabilitar');
+            classeControle1.classList.remove('controle1');
+            classeControle2.classList.remove('desabilitar');
+            classeControle2.classList.remove('controle2');
             controleDeImg = ''
+            back2 = back
+            front2 = front
+            setTimeout(desvirar,1000)
         }
     }
-    console.log(controleDeImg);
-    console.log(testando);
-    setTimeout(virar,270,elemento);  
 }
-
 
 function virar(elemento) {
-    const back=document.querySelector('.gambiarra>.back');
-    const front=document.querySelector('.gambiarra>.front');
+    back=document.querySelector('.gambiarra>.back');
+    front=document.querySelector('.gambiarra>.front');
+    back.classList.add('hide');
+    front.classList.remove('hide');   
     elemento.classList.remove('gambiarra');
-    back.classList.toggle('hide');
-    front.classList.toggle('hide');   
 }
 
+function desvirar() {
+    setTimeout(desvirarTempo,270)
+    classeControle1.classList.toggle('virar')
+    classeControle2.classList.toggle('virar')
+}
+
+function desvirarTempo() {
+    back.classList.remove('hide');
+    front.classList.add('hide');
+    back2.classList.remove('hide');
+    front2.classList.add('hide');
+}
